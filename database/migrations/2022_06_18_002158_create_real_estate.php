48bc5dc9b491058d95905cf799def9204a314eea
Gadius <gadlapaz@hotmail.com>
2022-06-17 23:33:16 -0600

    table name fix

--- database/migrations/2022_06_18_002158_create_real_estate.php
+++ database/migrations/2022_06_18_002158_create_real_estate.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('real_estate', function (Blueprint $table) {
+        Schema::create('real_estates', function (Blueprint $table) {
             $table->id();
 
             $table->string('name',128);	
