99d7afea13f0a72b6e4a90b94049d74ef0efe5b5
Gadius <gadlapaz@hotmail.com>
2022-06-17 22:28:13 -0600

    RealEstate migration created, default auth scaffolding backup on new carpet for future test

---
+++ database/migrations/2022_06_18_002158_create_real_estate.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('real_estate', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name',128);	
+            $table->enum('real_state_type', array('house', 'department','land','commercial_ground'));	
+            $table->string('street',128);	
+            $table->string('external_number',12);	
+            $table->string('internal_number',12)->nullable();
+            $table->string('neighborhood',128);	
+            $table->string('city',64);	
+            $table->string('country',2);	
+            $table->integer('rooms');	
+            $table->decimal('bathrooms');	
+            $table->string('comments',128)->nullable();	
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('real_estate');
+    }
+};
