010d8400b8d62a77c09f80602ca3b0c3025d872e
Gadius <gadlapaz@hotmail.com>
2022-06-18 20:55:31 -0600

    apiResource instead resource on routes to get rid of Create and Edit

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::resource('realestate', RealEstateController::class);
+Route::apiResource('realestate', RealEstateController::class);
